0d979c415cd1fc6867521570e18d4c353fc8eff7
Kevin Niu <kev.niu@gmail.com>
2016-03-02 16:26:58 -0800

    Add group_project_solution.js

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,27 @@
+/*
+Based on the tests, write user stories that describe what the code needs to do. User stories take the following format:
+
+As a user, I want to...
+
+The user stories should be easily translated into pseudocode by the next person in your group. However, a user story is NOT pseudocode -- it should describe the experience of someone using the function. Words like array or loop or any word not known to a non-technical person should not be used. You can specify the function name.
+*/
+
+// Sum User Story:
+//
+// As a user, I want to be able to calculate the sum of a group of numbers.
+//
+// ------------------------------------------------------------------------
+
+
+// Mean User Story:
+//
+// As a user, I want to be able to calculate the mean value of a group of numbers.
+//
+// ------------------------------------------------------------------------
+
+
+// Median User Story:
+//
+// As a user, I want to be able to find the median in a group of numbers.
+//
+// ------------------------------------------------------------------------
